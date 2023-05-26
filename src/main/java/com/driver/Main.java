21e2a487b704426f6cd111c3a021d1422340f856
Divyanshu Joshi <divyanshujoshi1012@gmail.com>
2023-05-26 23:53:09 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,8 +2,5 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
-        CurrentAccount ca = new CurrentAccount("Ramesh",20000, "11694684646");
-        System.out.println(ca.getTradeLicenseId());
     }
 }
